71ca872238d44c57c651b09a1a2acff9ae9f8cf7
sunnyleeyun <sunnylee945@hotmail.com>
2023-11-15 22:12:24 -0500

    feat: add forecast weather service

--- WeatherApp/Models/WeatherForecast.swift
+++ WeatherApp/Models/WeatherForecast.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct WeatherForecast: Codable {
+    let properties: WeatherForecastProperty
+}
+
+struct WeatherForecastProperty: Codable {
+    let periods: [WeatherForecastPeriod]
+}
+
+struct WeatherForecastPeriod: Codable {
+    let number: Int
+    let startTime: String
+    let endTime: String
+    let temperature: Int
+    let shortForecast: String
+    let detailedForecast: String
+}
--- WeatherApp/Services/WeatherService.swift
+++ WeatherApp/Services/WeatherService.swift
@@ -11,10 +11,10 @@ import CoreLocation
 protocol WeatherFetching {
     func fetchWeatherOverview(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completion: @escaping (WeatherOverviewProperty?, Error?) -> Void)
     
+    func fetchWeatherForecast(gridId: String, gridX: Int, gridY: Int, completion: @escaping (WeatherForecastProperty?, Error?) -> Void)
 }
 
 class WeatherService: WeatherFetching {
-    
     
     private var networkManager: NetworkManager
     private let baseUrl = "https://api.weather.gov/"
@@ -37,5 +37,22 @@ class WeatherService: WeatherFetching {
             }
         }
     }
+    
+    func fetchWeatherForecast(gridId: String, gridX: Int, gridY: Int, completion: @escaping (WeatherForecastProperty?, Error?) -> Void) {
+        guard let url = URL(string: "\(baseUrl)/gridpoints/\(gridId)/\(gridX),\(gridY)/forecast") else {
+            fatalError("Invalid URL")
+        }
+        
+        networkManager.request(fromURL: url, httpMethod: .get) { (result: Result<WeatherForecast, Error>) in
+            switch result {
+            case .success(let weatherForecast):
+                completion(weatherForecast.properties, nil)
+            case .failure(let error):
+                debugPrint(error.localizedDescription)
+                completion(nil, error)
+            }
+        }
+    }
+    
     
 }
